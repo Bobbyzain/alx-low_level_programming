1635a6d3a18f3a02a9ac04b067e10d87a0a1d5e9
Abdulhameed Abu <Abhameed419@gmail.com>
2023-02-16 20:11:33 -0800

    Additional corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main (void)
+{
+    char mystr[] = "Programming is like building a multilingual puzzle";
+    puts(mystr);
+    return (0);
+}
