59692951366b92f3ca5c689b53541651fd565477
Abdulhameed Abu <Abhameed419@gmail.com>
2023-02-19 09:52:54 -0800

    More for loop statements

---
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * function to print out the alphabet in lowercase and reverse
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x <= 'a'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
