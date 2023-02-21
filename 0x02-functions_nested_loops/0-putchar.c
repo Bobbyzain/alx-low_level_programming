73f3a3664dd72de51cc228633612579875de75e7
Abdulhameed Abu <Abhameed419@gmail.com>
2023-02-21 12:21:48 -0800

    printing characters

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
