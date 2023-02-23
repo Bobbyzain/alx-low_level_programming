71d5ddfa6cf447385b9be4a0c945de87b5eff851
Abdulhameed Abu <Abhameed419@gmail.com>
2023-02-22 19:51:13 -0800

    Hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
- * putcharPrinter - nested function
+ * main - Entry point to print the putchar output
+ *
  * Return: 0 Always (success)
  */
 
 int main(void)
 {
-	/**
-	 * putcharPrinter - to print the putchar output
-	 *
-	 * Return: 0 Always (success)
-	 */
-	int putcharPrinter(void)
-	{
-		_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');
-		return (0);
-	}
-	putcharPrinter();
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
