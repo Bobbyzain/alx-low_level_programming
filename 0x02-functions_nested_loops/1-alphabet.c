8f01877e7bfb35a3478b7dba9e54a40c76136968
Abdulhameed Abu <Abhameed419@gmail.com>
2023-02-22 20:07:23 -0800

    calling my function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+
+void print_alphabet(void)
+{
+	int x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
