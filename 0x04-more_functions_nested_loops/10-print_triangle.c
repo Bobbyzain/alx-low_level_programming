40469935cfcd30cb26d9cd97b8c7d0fa0071adf3
Abdulhameed Abu <Abhameed419@gmail.com>
2023-02-24 06:08:20 -0800

    printing triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ *
+ * @size: my integer variable counting the amount of print times
+ *
+ * Return: 0 Always (success)
+ */
+
+void print_triangle(int size)
+{
+	int m;
+	int o;
+	int p = size;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		while (size > 0)
+		{
+			m = 0;
+			while (m < size)
+			{
+				_putchar(' ');
+				m++;
+			}
+			o = p - size;
+			while (o >= 0)
+			{
+				_putchar('#');
+				o--;
+			}
+			_putchar('\n');
+			size--;
+		}
+}
