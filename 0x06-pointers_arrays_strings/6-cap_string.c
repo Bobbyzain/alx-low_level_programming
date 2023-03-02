e84c177b8e3bb6632425cd4bb52c592a5e61cff5
Abdulhameed Abu <Abhameed419@gmail.com>
2023-03-01 19:08:34 -0800

    Hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,14 +3,14 @@
 
 /**
  * cap_string - My special function
- * @s: collecting the strings to change to uppercase
+ * @c: collecting the strings to change to uppercase
  * Return: char output consisting of uppercase string
  */
 
-char *cap_string(char *s)
+char *cap_string(char *c)
 {
 	int i;
-	char *t = s;
+	char *t = c;
 
 	for (i = 0; t[i] != '\0'; i++)
 	{
