c8fc11ef7c99a814a7b12dcbea92ef3b22c5c131
Abdulhameed Abu <Abhameed419@gmail.com>
2023-03-06 18:54:47 -0800

    Creating my own memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - My memory set function
+ * @s: Pointer to the memory in question
+ * @b: The constant byte to fill the memory with
+ * @n: The number of address to fill
+ * Return: Pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*s = b;
+		i++;
+		s++;
+	}
+	return (s);
+}
