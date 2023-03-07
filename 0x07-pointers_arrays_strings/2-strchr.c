f8d1c5b6c4aad11f4c9f5977e4b7120f43030b2a
Abdulhameed Abu <Abhameed419@gmail.com>
2023-03-06 20:21:45 -0800

    Hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,9 +2,10 @@
 
 /**
  * _strchr - My string parsing function
+ * Description: To find & return a pointer 'r' to a character within a string
  * @s: Pointer to the string I want to parse through
  * @c: The character whose occurence I am searching for
- * return: Pointer to the 1st occurence of c in s
+ * Return: Pointer to the 1st occurence of c in s else NULL
  */
 
 char *_strchr(char *s, char c)
@@ -12,7 +13,7 @@ char *_strchr(char *s, char c)
 	int i = 0;
 	char *r;
 
-	for (;s[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
