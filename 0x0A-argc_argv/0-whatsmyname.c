8f28194ded58bfc29e73116580a3edf951148208
Abdulhameed Abu <Abhameed419@gmail.com>
2023-03-11 06:50:00 -0800

    Using arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: variable count
+ * @argv: arguments
+ *
+ * Return: The name of the program
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
