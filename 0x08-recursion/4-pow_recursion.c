f3be92d133678479e5a39e6dd0a6dc7b97b74c7b
Bobbyzain <abhameed419@gmail.com>
2023-06-08 14:15:38 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-pow_recursion.c -o 4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - To use recursion to find the power of a number
+ * @x: base value number to be raised to another
+ * @y: power number to raise the base value by
+ * Return: Always int.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int i;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+	{
+		i = x * _pow_recursion(x, y - 1);
+		return (i);
+	}
+}
+
