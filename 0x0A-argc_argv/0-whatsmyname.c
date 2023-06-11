c558c4678c4d9c654f904c97b43ac431e324c399
Bobbyzain <abhameed419@gmail.com>
2023-06-11 15:24:55 -0700

    Using argc and argv variables

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main: Entry point for my program
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
