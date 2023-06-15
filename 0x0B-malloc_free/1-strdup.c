d4aff48da3a41a69dd7d290f18b0bfef56514554
Bobbyzain <abhameed419@gmail.com>
2023-06-15 16:18:09 -0700

    malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - special function
+ * @*str: a pointer of xters in the array
+ * 
+ * Return: Null if size is 0 else pointer to array
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *t;
+
+	t = malloc(*str);
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		t[i] = str[i];
+		i++;
+	}
+
+	return (t);
+}
