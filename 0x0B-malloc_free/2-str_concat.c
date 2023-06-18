df4393a89492613646fcb3411d0501a40754e57c
Bobbyzain <abhameed419@gmail.com>
2023-06-18 04:43:22 -0700

    Hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,53 +4,58 @@
 #include <limits.h>
 
 /**
+ * null_corrector - special function
+ *
+ * Return: Null if size is 0 else pointer to array
+ */
+char *null_corrector(void)
+{
+	char *v;
+
+	v = malloc(sizeof(char) * 1);
+	if (v ==  NULL)
+		exit(1);
+	v[0] = '\0';
+	return (v);
+	free(v);
+}
+/**
  * str_concat - special function
  * @s1: a pointer to xters in the array
  * @s2: a pointer to xters in the array
  *
  * Return: Null if size is 0 else pointer to array
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	char *t;
+	int i = 0, j = 0, k = 0;
+	char *t, *u, *y;
 
-	if (s1 == NULL || s2 ==  NULL)
-	{
-		s1 = '\0';
-		s2 = '\0';
-	}
-
-	while (s1[i] != '\0')
-        {
+	if (s1 == NULL)
+		u = null_corrector();
+	else
+		u = s1;
+	if (s2 == NULL)
+		y = null_corrector();
+	else
+		y = s2;
+	while (u[i] != '\0')
 		i++;
-	}
-	while (s2[j] != '\0')
-	{
+	while (y[j] != '\0')
 		j++;
-	}
 	t = malloc(sizeof(char) * (i + j + 1));
-
 	if (t == NULL)
 		exit(1);
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
+	for (i = 0; u[i] != '\0'; i++)
 	{
-		t[k] = s1[i];
-		i++;
+		t[k] = u[i];
 		k++;
 	}
-	while (s2[j] != '\0')
+	for (j = 0; y[j] != '\0'; j++)
 	{
-		t[k] = s2[j];
-		j++;
+		t[k] = y[j];
 		k++;
 	}
-
 	return (t);
 	free(t);
 }
