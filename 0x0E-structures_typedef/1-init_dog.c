e384cdbaacdd5d7dde9a2351a4c22b047c06adb6
Bobbyzain <abhameed419@gmail.com>
2023-06-25 01:10:47 -0700

    Hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,7 +17,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	/*d = malloc(sizeof(struct dog));*/
 	if (d == NULL)
 	{
-		printf("OK\n");
+		printf("Ok\n");
 		exit(1);
 	}
 	d->name = name;
