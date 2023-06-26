698ecba8386b7a0062116c88ed2af48405fa45cf
Bobbyzain <abhameed419@gmail.com>
2023-06-26 12:16:22 -0700

    Hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,15 +14,41 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int i, j, x;
 
+	i = 0;
+	while (name[i] != '\0')
+		i++;
+	j = 0;
+	while (owner[j] != '\0')
+		j++;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	/* Allocating Memory for new dog structure */
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc((i + 1) * sizeof(char));
+	if (d->name == NULL)
 	{
 		free(d);
-		exit(1);
+		return (NULL);
 	}
-	d->name = name;
+
+	d->owner = malloc((j + 1) * sizeof(char));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
+	for (x = 0; x < i; x++)
+		d->name[x] = name[x];
+	for (x = 0; x < j; x++)
+		d->owner[x] = owner[x];
 	d->age = age;
-	d->owner = owner;
 	return (d);
 }
