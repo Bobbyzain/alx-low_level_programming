53d92c695c415d9357a746116c176d2cc7115c79
Bobbyzain <abhameed419@gmail.com>
2023-08-04 20:46:05 -0700

    Hot fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,38 +7,44 @@
  * @argv: arguments
  * Return: 0
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc,  char *argv[])
 {
-	int count = 1, i = 0, change = 0, rs = atoi(argv[count]);
+	int count = 1, i = 0, change = 0, rs;
 	int coin[5] = {25, 10, 5, 2, 1};
 
-	if (argc == 2)
-	{
-		while (i < 5)
-		{
-			if (rs >= coin[i])
-			{
-				if (rs % coin[i] > 0)
-				{
-					change += rs / coin[i];
-					rs = rs % coin[i];
-					i++;
-				}
-				else
-				{
-					change += rs / coin[i];
-					break;
-				}
-			}
-			else
-				i++;
-		}
+	rs = atoi(argv[count]);
+	if (rs <= 0)
 		printf("%d\n", change);
-	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		if (argc == 2)
+		{
+			while (i < 5)
+			{
+				if (rs >= coin[i])
+				{
+					if (rs % coin[i] > 0)
+					{
+						change += rs / coin[i];
+						rs = rs % coin[i];
+						i++;
+					}
+					else
+					{
+						change += rs / coin[i];
+						break;
+					}
+				}
+				else
+					i++;
+			}
+			printf("%d\n", change);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
 	return (0);
 }
