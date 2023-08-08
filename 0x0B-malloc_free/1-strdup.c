7791f39168764f90e52ab9eccd9d687145666b30
Bobbyzain <abhameed419@gmail.com>
2023-08-07 21:58:00 -0700

    Hot fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,10 +11,12 @@ char *_strdup(char *str)
 	char *s;
 	int j = 0, i = 0;
 
+	if (str == NULL)
+		return (NULL);
 	while (str[i] != '\0')
 		i++;
 
-	s = malloc(i * sizeof(char));
+	s = malloc(i + 1 * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,8 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k, l = 0;
 	char *s;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
