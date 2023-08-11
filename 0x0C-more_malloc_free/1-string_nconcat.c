1e4bcfd3b16bff3d47c363a3e63d88a350f9cbe9
Bobbyzain <abhameed419@gmail.com>
2023-08-10 22:06:38 -0700

    a function that concatenates two strings.
        Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n);
        The returned pointer shall point to a newly allocated space in memory, which contains s1, followed by the first n bytes of s2, and null terminated
        If the function fails, it should return NULL
        If n is greater or equal to the length of s2 then use the entire string s2
        if NULL is passed, treat it as an empty string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * null_corrector - special function
+ *
+ * Return: Null if size is 0 else pointer to array
+ */
+
+char *null_corrector(void)
+{
+	char *v;
+
+	v = malloc(sizeof(char) * 1);
+	if (v == NULL)
+		exit(1);
+	v[0] = '\0';
+	return (v);
+	free(v);
+}
+/**
+ * string_nconcat - to concatenate 2 strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @n: size of s2 to concatenate
+ * Return: Pointer to a new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s, *t, *u;
+	unsigned int i = 0, j = 0, k = 0;
+
+	if (s1 == NULL)
+		t = null_corrector();
+	else
+		t = s1;
+	if (s2 == NULL)
+		u = null_corrector();
+	else
+		u = s2;
+	while (t[i] != '\0')
+		i++;
+	while (u[j] != '\0')
+		j++;
+	if (n > j)
+		n = j;
+	s = malloc(sizeof(char) * (i + n + 1));
+	if (s == NULL)
+		exit(1);
+	for (i = 0; t[i] != '\0'; i++)
+	{
+		s[k] = t[i];
+		k++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		s[k] = u[j];
+		k++;
+	}
+	return (s);
+	free(s);
+}
