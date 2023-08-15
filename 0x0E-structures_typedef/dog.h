8df7ec0279c8c61ca9bbadfbb9b44c62931c06dd
Bobbyzain <abhameed419@gmail.com>
2023-08-14 17:42:59 -0700

    Hot fix

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a structure for defining a dog
+ * @name: 1st descriptor for its name
+ * @age: 2nd descriptor for its age
+ * @owner: 3rrd descriptor for its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+	/* dog - a structure */
+};
+#endif
