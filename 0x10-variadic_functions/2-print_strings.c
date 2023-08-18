d4780188fd1f136f669b442a07a40fa6db2a0454
Bobbyzain <abhameed419@gmail.com>
2023-08-18 02:23:57 -0700

    a function that prints strings, followed by a new line.
        Prototype: void print_strings(const char *separator, const unsigned int n, ...);
        where separator is the string to be printed between the strings
        and n is the number of strings passed to the function
        You are allowed to use printf
        If separator is NULL, don’t print it
        If one of the string is NULL, print (nil) instead
        Print a new line at the end of your function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - variadic function that prints strings and separators
+ * @separator: to be printed between strings
+ * @n: number of argument parameters to be provided
+ * Return: Nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *p;
+
+	va_start(ap, n);
+	if (n == 0)
+	{
+		printf("\n");
+		exit(1);
+	}
+	else
+	{
+		if (separator == NULL)
+		{
+			for (i = 0; i < n - 1; i++)
+			{
+				if (va_arg(ap, char *) == NULL)
+					printf("(nil)");
+				else
+					printf("%s", va_arg(ap, char *));
+			}
+		}
+		else
+		{
+			for (i = 0; i < n - 1; i++)
+			{
+				if (va_arg(ap, char *) == NULL)
+					printf("(nil)");
+				else
+					printf("%s%s", va_arg(ap, char *), separator);
+			}
+		}
+		if (i == n - 1)
+		{
+			p = va_arg(ap, char *);
+			if (p == NULL)
+			{
+				p = "(nil)";
+			}
+			printf("%s\n", p);
+		}
+	}
+	va_end(ap);
+}
