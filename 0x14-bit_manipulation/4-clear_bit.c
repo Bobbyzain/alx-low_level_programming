bda558fda8cf932ae0454372f44ff30158569c86
Bobbyzain <abhameed419@gmail.com>
2023-09-03 10:43:11 -0700

    Hot fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,14 +10,18 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int q = *n, k = 1;
-	unsigned int j = index, i = 63;
+	unsigned int j = index, r = 63, i = 0;
 
-	/*while (q > (1 << i))*/
-	/*	i++;*/
+	while (q > (k << i))
+		i++;
+	if (i < j)
+		return (1);
 	if (q == 0)
 		return (-1);
-	if (index > i)
+	if (index > r)
 		return (-1);
+	if ((q >> j & 1) == 0)
+		return (1);
 	*n = (q ^ (k << j));
 	return (1);
 }
