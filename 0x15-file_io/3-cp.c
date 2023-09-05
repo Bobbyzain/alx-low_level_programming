549b4276b02aa056e885cb9b708e330659438d62
Bobbyzain <abhameed419@gmail.com>
2023-09-05 07:03:01 -0700

    Hot fix of the copy function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	int fpf, bytes_read, written, close_res;
+	int fpt, fpf, bytes_read, written, close_res;
 	char *buffer;
 
 	if (argc != 3)
@@ -25,14 +25,19 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 	bytes_read = read(fpf, buffer, 1024);
-	written = write_copied(bytes_read, buffer, argv[2]);
-	if (fpf == -1 || bytes_read == -1)
-	{
-		close(fpf);
-		free(buffer);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	fpt = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	do {
+		written = write_copied(bytes_read, buffer, fpt, argv[2]);
+		if (fpf == -1 || bytes_read == -1)
+		{
+			close(fpf);
+			free(buffer);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		bytes_read =  read(fpf, buffer, 1024);
+		fpt = open(argv[2], O_WRONLY | O_APPEND);
+	} while (bytes_read > 0);
 	close_res = close(fpf);
 	if (close_res != 0)
 	{
@@ -46,17 +51,17 @@ int main(int argc, char *argv[])
  * write_copied - write what was copied
  * @read_t: the tracker for the content read
  * @buf: the size of the content to be written
- * @f_name: the name of the file to copy content to
+ * @f_des: the file descriptor
+ * @f_name: the name of the file
  * Return: Always int
  */
-int write_copied(int read_t, char *buf, char *f_name)
+int write_copied(int read_t, char *buf, int f_des, char *f_name)
 {
-	int written, fpt, close_res;
+	int written, fpt = f_des, close_res;
 
 	if (buf == NULL)
 		return (-1);
 
-	fpt = open(f_name, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	written =  write(fpt, buf, read_t);
 	if (fpt == -1 || written == -1)
 	{
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char *argv[]);
-int write_copied(int read_t, char *buf, char *f_name);
+int write_copied(int read_t, char *buf, int fpt, char *f_name);
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
