0b121c8cff50ecf7919d5adf31d84ad391793a84
Bobbyzain <abhameed419@gmail.com>
2023-09-05 06:00:44 -0700

    Fixing the stderr problem

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,25 +9,22 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	int i = 0, k = letters;
+	int fp, written;
+	int k = letters;
 	ssize_t j = 0;
 	char *str;
 
 	if (filename == NULL)
 		return (0);
-
 	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 		return (0);
-
 	str = malloc(sizeof(char) * k);
 	if (str == NULL)
 	{
 		close(fp);
 		return (0);
 	}
-
 	j = read(fp, str, k);
 	if (j == -1)
 	{
@@ -35,16 +32,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fp);
 		return (0);
 	}
-	for (; i < j; i++)
-		_putchar(str[i]);
-	if (i == -1 || i != j)
+	written = write(STDOUT_FILENO, str, j);
+	if (written == -1 || written != j)
 	{
 		close(fp);
 		free(str);
 		return (0);
 	}
-
 	free(str);
 	close(fp);
-	return (j);
+	return (written);
 }
