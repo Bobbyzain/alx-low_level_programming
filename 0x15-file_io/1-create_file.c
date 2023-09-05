73c17cdc0f3447234a0428f6a3aadff9899ad34b
Bobbyzain <abhameed419@gmail.com>
2023-09-05 06:08:50 -0700

    solving the extra concatenation problem

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,8 @@ int create_file(const char *filename, char *text_content)
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
-	for (; text_content[i] != '\0'; i++)
+	while (text_content[i] != '\0')
+		i++;
 	written = write(fp, text_content, i);
 	if (written == -1)
 	{
